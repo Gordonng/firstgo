f0aaf2e2da5d61bcd7e312982bd3d30dab753879
gordonng <ggg222999@gmail.com>
2022-05-30 21:57:02 +0800

    5th

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
-package main
-
-import (
-   "log"
-   "net/http"
-)
-
-type Server struct{}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-   w.WriteHeader(http.StatusOK)
-   w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "hello world"}`))
-}
-
-func main() {
-   s := &Server{}
-   http.Handle("/", s)
-   log.Fatal(http.ListenAndServe(":8080", nil))
+ package array_utils
+ //求最大子序列和 （就是说子序列加起来和最大）
+ func FindMaxSeqSum(array []int) int {
+   SeqSum := make([]int, 0) // 存储子序列和
+   // 初始子序列和为 数组下标为0的值
+   SeqSum = append(SeqSum, array[0])
+   for i := 1; i < len(array); i++ {
+      if array[i] > SeqSum[i-1]+array[i] {
+         SeqSum = append(SeqSum, array[i])
+      } else {
+         SeqSum = append(SeqSum, SeqSum[i-1]+array[i])
+      }
+   }
+   max := SeqSum[0]
+   for j := 1; j < len(SeqSum); j++ {
+      if SeqSum[j] > SeqSum[j-1] {
+         max = SeqSum[j]
+      }
+   }
+   fmt.Println(max) //打印结果
+   return max
 }
--- main_test.go
+++ main_test.go
@@ -1,20 +1,35 @@
-package main
+package array_utils
 
 import (
-   "log"
-   "net/http"
+   "fmt"
+   "os"
+   "testing"
 )
-
-type Server struct{}
-
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-   w.WriteHeader(http.StatusOK)
-   w.Header().Set("Content-Type", "application/json")
-   w.Write([]byte(`{"message": "hello world"}`))
+//TestMain会在下面所有测试方法执行开始前先执行，一般用于初始化资源和执行完后释放资源
+func TestMain(m *testing.M) {
+   fmt.Println("初始化资源")
+   result := m.Run() //运行go的测试，相当于调用main方法
+   fmt.Println("释放资源")
+   os.Exit(result) //退出程序
+}
+//单元测试
+func TestFindMaxSeqSum(t *testing.T) {
+   sum := FindMaxSeqSum([]int{1, 3, -9, 6, 8, -19})
+   if sum == 14 {
+      t.Log("successful")
+   } else {
+      t.Error("failed")
+   }
+}
+//基准测试
+func BenchmarkFindMaxSeqSum(b *testing.B) {
+   for i := 0; i < b.N; i++ {
+      FindMaxSeqSum([]int{1, 3, -9, 6, 8, -19})
+   }
+}
+//这个验证的是FindMaxSeqSum方法控制台输出的max和OutPut后面的14是否一致，如果相同，则表示验证通过，否则测试用例失败
+func ExampleFindMaxSeqSum() {
+   FindMaxSeqSum([]int{1, 3, -9, 6, 8, -19})
+   // OutPut: 14
 }
 
-func main() {
-   s := &Server{}
-   http.Handle("/", s)
-   log.Fatal(http.ListenAndServe(":8080", nil))
-}
